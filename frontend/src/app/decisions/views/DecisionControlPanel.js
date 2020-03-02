7c3a443a965c237db29396eb6f08db963e546f7a
atroudi <atroudi@qf.org.qa>
2020-03-02 17:31:31 +0300

    add control accordion

--- frontend/src/app/decisions/views/DecisionControlPanel.js
+++ frontend/src/app/decisions/views/DecisionControlPanel.js
@@ -2,6 +2,7 @@ import {Box} from "adminlte";
 import React, {PureComponent} from 'react';
 import { Range } from 'react-range';
 import Switch from "react-switch";
+import {PanelGroup, Panel} from "react-bootstrap";
 
 const defaultContainer = ({children}) => <div className="control-panel">{children}</div>;
 
@@ -83,6 +84,42 @@ class RangeControl extends PureComponent {
     );
   }
 }
+
+class SwitchControl extends React.Component {
+  constructor(props){
+    super();
+    this.state = {
+      checked: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.props.handleSwitchChange();
+    this.setState({ checked });
+  }
+
+  render(){
+    return(
+      <Switch
+                      checked={this.state.checked}
+                      onChange={this.handleChange}
+                      onColor="#bdebd6"
+                      onHandleColor="#378663"
+                      handleDiameter={20}
+                      uncheckedIcon={false}
+                      checkedIcon={false}
+                      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                      activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                      height={10}
+                      width={32}
+                      className="react-switch"
+                      id="material-switch"
+                    />
+    );
+  }
+}
+
 export default class ControlPanel extends PureComponent {
 
   constructor(props) {
@@ -95,71 +132,114 @@ export default class ControlPanel extends PureComponent {
       demands: [demand],
       resultPanelswitch: false
     };
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  // componentWillMount(){
-  //   this.setState({values : [this.props.params.demand]});
-  // }
-
-  handleChange(checked) {
-    this.setState({ resultPanelswitch: checked });
-    this.props.onSwitchResultPanel();
-  }
   render() {
     const {time} = this.props;
     
     return (
         <div className="text-center" >
-        <Box.Wrapper   >
-            <Box.Header>
-                    <Box.Title>Decision Input</Box.Title>
-                </Box.Header>
-                <Box.Body>
+          <PanelGroup accordion id="accordion-uncontrolled-example" defaultActiveKey="1">
+          <Panel eventKey="1">
+              <Box.Wrapper>
+                  <Box.Header>
+                          <Panel.Title toggle>Decision Input</Panel.Title>
+                      </Box.Header>
+              <Panel.Body collapsible>
+              <Box.Body>
+              <div className="text-left" >
+                <p>Set parameters for crop decision prediction
+                  <br/>
+                  from <b>2020-02-19 {time}</b> to <b>2020-02-19 {time}</b>
+                </p>
+                <hr />
+              </div>
+              <div className="text-left" >
+                <label>Demand</label>
+                <br/>
+                <RangeControl {...this.props} min={0} max={this.state.demands[0]*3} step={1000} values={this.state.demands} variable="demand" />
+              </div>
+              <br/>
+              <div className="text-left" >
+                <label>Temperature</label>
+                <br/>
+                <RangeControl {...this.props} min={-5} max={5} step={0.1} values={[0]} variable="temperature" />
+              </div>
+              <hr />
+              <div className="text-left" >
+                <label htmlFor="material-switch1">
+                    <SwitchControl handleSwitchChange={this.props.onSwitchResultPanel} />
+                    <span style={{paddingTop: '100px'}}> Result Panel </span>
+                    </label>
+                    </div>
 
-        <div className="text-left" >
-          <p>Set parameters for crop decision prediction
-            <br/>
-             from <b>2020-02-19 {time}</b> to <b>2020-02-19 {time}</b>
-          </p>
-          <hr />
-        </div>
-        <div className="text-left" >
-          <label>Demand</label>
-          <br/>
-          <RangeControl {...this.props} min={0} max={this.state.demands[0]*3} step={1000} values={this.state.demands} variable="demand" />
-        </div>
-        <br/>
-        <div className="text-left" >
-          <label>Temperature</label>
-          <br/>
-          <RangeControl {...this.props} min={-5} max={5} step={0.1} values={[0]} variable="temperature" />
-        </div>
-        <hr />
-        <div className="text-left" >
-          <label htmlFor="material-switch">
-            <Switch
-                checked={this.state.resultPanelswitch}
-                onChange={this.handleChange}
-                onColor="#bdebd6"
-                onHandleColor="#378663"
-                handleDiameter={20}
-                uncheckedIcon={false}
-                checkedIcon={false}
-                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-                height={10}
-                width={32}
-                className="react-switch"
-                id="material-switch"
-              />
-              <span style={{paddingTop: '100px'}}>     Result Panel </span>
-              </label>
+              </Box.Body>
+              </Panel.Body>
+              </Box.Wrapper>
+          </Panel>
+
+          <Panel eventKey="2">
+              <Box.Wrapper>
+                  <Box.Header>
+                          <Panel.Title toggle>Disaster scenarios</Panel.Title>
+                      </Box.Header>
+              <Panel.Body collapsible>
+              <Box.Body>
+              <div className="text-left" >
+                <p>Possible scenarios that may affect decsion prediction
+                  <br/>
+                  from <b>2020-02-19 {time}</b> to <b>2020-02-19 {time}</b>
+                </p>
+                <hr />
+              </div>
+      
+              <div className="text-left" >
+                <label htmlFor="material-switch2">
+                  <SwitchControl handleSwitchChange={t => console.log("blockade switch enabled")} />
+                  <span style={{paddingTop: '100px'}}> Blockade </span>
+                </label>
+                <label htmlFor="material-switch3">
+                    <SwitchControl handleSwitchChange={t => console.log("Fire switch enabled")} />
+                    <span style={{paddingTop: '100px'}}> Fire </span>
+                </label>
+                {/* <label htmlFor="material-switch">
+                    <SwitchControl handleChange={t => console.log("Flood switch enabled")} />
+                    <span style={{paddingTop: '100px'}}> Flood </span>
+                </label> */}
               </div>
 
-        </Box.Body>
+              </Box.Body>
+              </Panel.Body>
+              </Box.Wrapper>
+          </Panel>
 
-        </Box.Wrapper>
+          <Panel eventKey="3">
+              <Box.Wrapper>
+                  <Box.Header>
+                          <Panel.Title toggle>Fields Visualization</Panel.Title>
+                      </Box.Header>
+              <Panel.Body collapsible>
+              <Box.Body>
+              <div className="text-left" >
+                <p>In depth field visualization of local farms
+                  <br/>
+                  from <b>2020-02-19 {time}</b> to <b>2020-02-19 {time}</b>
+                </p>
+                <hr />
+              </div>
+      
+              <div className="text-left" >
+                <label htmlFor="material-switch4">
+                  <SwitchControl handleSwitchChange={t => console.log("blockade switch enabled")} />
+                  <span style={{paddingTop: '100px'}}> Blockade </span>
+                </label>
+              </div>
+
+              </Box.Body>
+              </Panel.Body>
+              </Box.Wrapper>
+          </Panel>
+        </PanelGroup>
         </div>
     );
   }
