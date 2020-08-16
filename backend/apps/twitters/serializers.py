0800c3d76eac5465ca76b6f2224cfb77cffff0c8
atroudi <atroudi@qf.org.qa>
2020-08-16 13:09:39 +0300

    added tweets serializer

--- backend/apps/twitters/serializers.py
+++ backend/apps/twitters/serializers.py
@@ -1,23 +1,9 @@
-from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.apps import apps
+from .models import Twitter
 
-User = get_user_model()
-# Snippet = apps.get_model('snippets', 'Snippet')
-# Snippet = apps.get_model('records', 'Records')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    # appConfig = AppConfig()
-    # Snippet = appConfig.get_model(model_name='Snippet')
-    # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
+class TwitterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-
+        model = Twitter
         fields = (
-            'id', 'first_name', 'last_name', 'email', 'last_login',
-            'is_active', 'date_joined', 'last_updated','is_staff','is_superuser'
+            'id', 'twid', 'active','author', 'avatar', 'body','date', 'screen_name'
         )
-        default_permissions = ('add', 'change', 'delete', 'view_emailuser')
