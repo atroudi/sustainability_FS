1ccd77553543ed414c27a03113b72700f03d17e1
atroudi <atroudi@qf.org.qa>
2020-08-18 13:30:57 +0300

    change getTweet to getSentiment

--- backend/apps/twitters/sentimentAnalysisView.py
+++ backend/apps/twitters/sentimentAnalysisView.py
@@ -1,22 +1,56 @@
-from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse, Http404, JsonResponse
 import tweepy
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 from rest_framework.decorators import api_view
+import os
+import pickle
 
 consumer_key = 'qhwg88DbtCpCG2hQumqSKj3qp'
 consumer_secret = 'BZy237443Jj7hePJvSnRUFMePKCnrXVtEbhzXYQbDEwtUm8LQy'
 access_token = '3315982002-w8V3IgrJWXKjNHuKjqMWOj7dGsTqG2rZaQSl91Y'
 access_token_secret = 'LBkNkwRhZga9O3MokOIClPagFWGBx97DDo6RXvWFwqjrv'
 
+# Keras stuff
+global graph
+graph = tf.get_default_graph()
+project_root = os.path.dirname(os.path.abspath(__file__))
+model = load_model(project_root + '/resources/Sentiment_CNN_model.h5')
+MAX_SEQUENCE_LENGTH = 300
+
 # Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
+# loading tokenizer
+with open(project_root + '/resources/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def predict(text, include_neutral=True):
+    print("performing prediction")
+    # Tokenize text
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_SEQUENCE_LENGTH)
+    # Predict
+    score = model.predict([x_test])[0]
+    if(score >=0.4 and score<=0.6):
+        label = "Neutral"
+    if(score <=0.4):
+        label = "Negative"
+    if(score >=0.6):
+        label = "Positive"
+
+    return {"label" : label,
+        "score": float(score)}
+
 @api_view(["GET"])
-def gettweets(request):
+def getSentiment(request):
     tweets = []
     count=0
+    positive = 0
+    neutral = 0
+    negative = 0
     print("getting tweets")
     for tweet in tweepy.Cursor(api.search,q="#" + "covid19" + " -filter:retweets",rpp=5,lang="en", tweet_mode='extended').items(50):
         # print(tweet)
@@ -25,9 +59,17 @@ def gettweets(request):
         temp = {}
         temp["text"] = tweet.full_text
         temp["username"] = tweet.user.screen_name
-        # with graph.as_default():
-        #     prediction = predict(tweet.full_text)
-        temp["label"] = "label"
-        temp["score"] = "score"
+        with graph.as_default():
+            prediction = predict(tweet.full_text)
+        temp["label"] = prediction["label"]
+        temp["score"] = prediction["score"]
         tweets.append(temp)
-    return JsonResponse({"results": tweets});
+
+        if (prediction["label"] == "Positive"):
+            positive += 1
+        if (prediction["label"] == "Neutral"):
+            neutral += 1
+        if (prediction["label"] == "Negative"):
+            negative += 1
+
+    return JsonResponse({"positive": positive, "neutral": neutral, "negative": negative, "results": tweets});
--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ from backend.apps.users.registration.signup import signup
 from django.contrib import admin
 
 from .views import app, index
-from backend.apps.twitters.sentimentAnalysisView import gettweets
+from backend.apps.twitters.sentimentAnalysisView import getSentiment
 from django.conf import settings
 
 urlpatterns = [
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^api/v1/', include('records.urls')),
     url(r'^api/v1/', include('twitters.urls')),
     url(r'^', include('snippets.urls')),
-    url(r'^twitter/', gettweets, name='twitter'),
+    url(r'^twitter/', getSentiment, name='twitter'),
 
     url(r'^app/', app, name='app'),
     url('^auth/login/$', login, name='login'),
