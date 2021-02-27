83feb437d41a7488364c25d919929d3794420ecf
atroudi <atroudi@qf.org.qa>
2021-02-27 23:41:03 +0300

    added prod setting file and package fixes

---
+++ backend/settings/prod.py
@@ -0,0 +1,106 @@
+import os
+
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '))a)f_b#(lo_v@cov$o)g*r8a$7%#51+*)&ybpy3!qz-8e__e1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+
+        # DEVELOPMENT INIT DB
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'USER': 'migiwara',
+        # 'PASSWORD': 'root',
+        # 'HOST':'localhost',
+        # 'PORT':'5432',
+        # 'NAME': 'biosustainabilitydb',
+
+        # DOCKER INIT DB
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'migiwara',
+        'NAME': 'sustainabilitydb',
+        'PASSWORD': 'qatar123',
+        'HOST': 'db'
+    }
+}
+
+INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
+INSTALLED_APPS += (
+    'autofixture',
+)
+
+STATICFILES_DIRS.append(
+    os.path.join(BASE_DIR, os.pardir, 'frontend', 'build'),
+)
+
+APPEND_SLASH = True
+# trailing_slash = False
+# CSRF_COOKIE_SECURE = False
+
+
+# ********************* DOCKER CELERY CONFIG
+CELERY_BROKER_URL = 'redis://redis:6379'
+
+# CELERY STUFF
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+
+# ********************* DEVELOPMENT CELERY CONFIG
+# # CELERY CONFIG
+# CELERY_BROKER_URL = 'amqp://localhost'
+#
+# # CELERY STUFF
+# BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Africa/Nairobi'
+
+# if DEBUG:
+#    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+#    MIDDLEWARE_CLASSES += (
+#        'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    )
+#
+#    INSTALLED_APPS += (
+#        'debug_toolbar',
+#    )
+#
+#    DEBUG_TOOLBAR_PANELS = [
+#        'debug_toolbar.panels.versions.VersionsPanel',
+#        'debug_toolbar.panels.timer.TimerPanel',
+#        'debug_toolbar.panels.settings.SettingsPanel',
+#        'debug_toolbar.panels.headers.HeadersPanel',
+#        'debug_toolbar.panels.request.RequestPanel',
+#        'debug_toolbar.panels.sql.SQLPanel',
+#        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#        'debug_toolbar.panels.templates.TemplatesPanel',
+#        'debug_toolbar.panels.cache.CachePanel',
+#        'debug_toolbar.panels.signals.SignalsPanel',
+#        'debug_toolbar.panels.logging.LoggingPanel',
+#        'debug_toolbar.panels.redirects.RedirectsPanel',
+#    ]
+#
+#    DEBUG_TOOLBAR_CONFIG = {
+#        'INTERCEPT_REDIRECTS': False,
+#    }
+#
+# # Redis configuration
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient"
+#         },
+#         "KEY_PREFIX": "biosustainability"
+#     }
+# }
--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.devl")
-
+    if os.environ.get('DJANGO_PROD') == 'true':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.prod")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.devl")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
